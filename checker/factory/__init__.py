161c14061468c71e68479c2196436cee2b8ebd4a
piotrszymanski133 <63449627+piotrszymanski133@users.noreply.github.com>
2023-03-12 09:09:07 +0100

    Finished potential secrets in env checker

--- checker/factory/__init__.py
+++ checker/factory/__init__.py
@@ -2,8 +2,9 @@ import checker.base_checker
 import checker.container.restart_policy_checker
 import checker.container.cpu_and_memory_limits_checker
 import checker.container.db_container_has_volume_attached_checker
+import checker.container.secrets_in_env_checker
 import checker.volume.unused_volume_checker
+import checker.volume.sensitive_mountpoint_checker
 import checker.node.number_of_master_nodes_checker
 import checker.network.isolated_containers_checker
-import checker.volume.sensitive_mountpoint_checker
 import checker.log.errors_in_logs_checker

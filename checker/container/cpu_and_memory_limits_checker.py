84d2a959a077c887f2d2bed98a46f997bc6d1644
piotrszymanski133 <63449627+piotrszymanski133@users.noreply.github.com>
2023-05-29 11:57:17 +0200

    Change limits to work for services

--- checker/container/cpu_and_memory_limits_checker.py
+++ checker/container/cpu_and_memory_limits_checker.py
@@ -1,4 +1,5 @@
 from docker.models.containers import Container
+from docker.models.services import Service
 
 from checker.base_checker import BaseChecker
 from checker.result.checker_result import CheckerResult
@@ -6,25 +7,21 @@ from checker.result.checker_result import CheckerResult
 
 class CpuAndMemoryLimitsChecker(BaseChecker):
     def run_checker(self) -> CheckerResult:
-        tasks = self.docker_client.api.tasks(filters={'desired-state': 'running'})
+        services = self.docker_client.services.list()
         are_only_containers_with_limits = True
-        for task in tasks:
-            has_container_cpu_limit = self.__has_container_cpu_limit(task)
-            has_container_memory_limit = self.__has_container_memory_limit(task)
-            status = task.get('Status')
-            if 'ContainerStatus' not in status:
-                continue
-            container_id = status.get('ContainerStatus').get('ContainerID')
+        for service in services:
+            has_container_cpu_limit = self.__has_container_cpu_limit(service)
+            has_container_memory_limit = self.__has_container_memory_limit(service)
 
             if not has_container_cpu_limit and not has_container_memory_limit:
-                self.logger.warning(f"No CPU and memory limits are set for the container {container_id}. Please set the "
+                self.logger.warning(f"No CPU and memory limits are set for the service {service.id}. Please set the "
                                   f"limits.")
                 are_only_containers_with_limits = False
             elif not has_container_cpu_limit:
-                self.logger.warning(f"No CPU limit is set for the container {container_id}. Please set the CPU limit.")
+                self.logger.warning(f"No CPU limit is set for the service {service.id}. Please set the CPU limit.")
                 are_only_containers_with_limits = False
             elif not has_container_memory_limit:
-                self.logger.warning(f"No memory limit is set for the container {container_id}. Please set the memory limit.")
+                self.logger.warning(f"No memory limit is set for the service {service.id}. Please set the memory limit.")
                 are_only_containers_with_limits = False
 
         if are_only_containers_with_limits:
@@ -34,14 +31,14 @@ class CpuAndMemoryLimitsChecker(BaseChecker):
         else:
             return CheckerResult.FAILED
 
-    def __has_container_memory_limit(self, task: dict) -> bool:
-        limits = self.__get_limits(task)
+    def __has_container_memory_limit(self, service: Service) -> bool:
+        limits = self.__get_limits(service)
         return "MemoryBytes" in limits
 
-    def __has_container_cpu_limit(self, task: dict) -> bool:
-        limits = self.__get_limits(task)
+    def __has_container_cpu_limit(self, service: Service) -> bool:
+        limits = self.__get_limits(service)
         return "NanoCPUs" in limits
 
-    def __get_limits(self, task: dict) -> dict:
-        resources = task.get('Spec').get('Resources')
+    def __get_limits(self, service: Service) -> dict:
+        resources = service.attrs.get('Spec').get('TaskTemplate').get('Resources')
         return resources.get('Limits') if 'Limits' in resources else {}

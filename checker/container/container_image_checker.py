bf584e3b47c295cdbe22051f3ea3e44af0d3da8c
piotrszymanski133 <63449627+piotrszymanski133@users.noreply.github.com>
2023-05-29 12:01:54 +0200

    Change imageChecker to work for services

--- checker/container/container_image_checker.py
+++ checker/container/container_image_checker.py
@@ -4,22 +4,14 @@ from checker.result.checker_result import CheckerResult
 
 class ContainerImageChecker(BaseChecker):
     def run_checker(self) -> CheckerResult:
-        tasks = self.docker_client.api.tasks(filters={'desired-state': 'running'})
+        services = self.docker_client.services.list()
         passed = True
-        for task in tasks:
-            image = task.get('Spec').get('ContainerSpec').get('Image')
-            status = task.get('Status')
-            if 'ContainerStatus' not in status:
-                continue
-            container_id = status.get('ContainerStatus').get('ContainerID')
+        for service in services:
+            image = service.attrs.get('Spec').get('TaskTemplate').get('ContainerSpec').get('Image')
             if 'latest' in image:
                 passed = False
-                self.logger.warning(f"Container {container_id} was started using image with latest tag - {image}."
+                self.logger.warning(f"Service {service.id} was started using image with latest tag - {image}."
                                     f" Please use other tag or sha256 hash of the image.")
-            elif ':' not in image:
-                passed = False
-                self.logger.warning(f"Container {container_id} was started using image without specifying tag - {image}."
-                                    f" Please use tag or sha256 hash of the image.")
 
         if passed:
             self.logger.info("All of the containers were started using proper tag or sha256 tag.")
